cef7d180714b54761d6b8f7b8de61c1c4b6d4801
Cameron Kenny <cam.e.kenny@gmail.com>
2021-01-23 13:38:36 +0000

    Setting up GUI

--- src/Player.java
+++ src/Player.java
@@ -1,10 +1,43 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Player {
-    
-    public static void main( String[] args )
-    {
+public class Player extends JFrame{
 
+    private int width;
+    private int height;
+    private Container contentPane;
+    private JTextArea message;
+    private JButton b1;
+    private JButton b2;
+    private JButton b3;
+    private JButton b4;
+
+    public Player(int w, int h) {
+        width = w;
+        height = h;
+        contentPane = this.getContentPane();
+        message = new JTextArea();
+        b1 = new JButton("1");
+        b2 = new JButton("2");
+        b3 = new JButton("3");
+        b4 = new JButton("4");
     }
+
+    public void setUpGUI() {
+        this.setSize(width, height);
+        this.setTitle("Turn-Based Game");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        contentPane.setLayout(new GridLayout(1, 5));
+        contentPane.add(message);
+        message.setText("Simple turn based game in Java");
+        message.setWrapStyleWord(true);
+        message.setEditable(false);
+        contentPane.add(b1);
+        contentPane.add(b2);
+        contentPane.add(b3);
+        contentPane.add(b4);
+        this.setVisible(true);
+    }
+
+    
 }
